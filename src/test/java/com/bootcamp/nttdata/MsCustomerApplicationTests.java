2545120f8383897b7f740fdb2e855b6b4d9d040a
Hector Huaman <hector.10.fisi@gmail.com>
2024-02-14 05:14:14 -0500

    Se creo el ms gateway

---
+++ src/test/java/com/bootcamp/nttdata/MsCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.nttdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
